36313e62abbf75d8fa43c44e94f5a9b61c6065a9
nt.ymt61@gmail.com <nt.ymt61@gmail.com>
2019-04-23 03:02:56 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8603d33d38bc6c96308a05a424404191.js"
+  "/portfolio/precache-manifest.b4c697bd93239b361e61ac36bf3facd4.js"
 );
 
 workbox.clientsClaim();
